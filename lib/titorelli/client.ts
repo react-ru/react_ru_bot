329794d928e271bded24b0d416a70963b5014a43
ychebotaev <yury.79120345101@gmail.com>
2025-01-13 22:29:59 +0500

    fix: request cas/predict scope

--- lib/titorelli/client.ts
+++ lib/titorelli/client.ts
@@ -5,5 +5,5 @@ export const client = createClient({
   clientId: process.env.TITORELLI_CLIENT_ID,
   clientSecret: process.env.TITORELLI_OAUTH_CLIENT_SECRET,
   modelId: process.env.TITORELLI_CLIENT_MODEL_ID,
-  scope: ['predict', 'train', 'exact_match/train', 'totems/train', 'cas/train'] as any
+  scope: ['predict', 'train', 'exact_match/train', 'totems/train', 'cas/predict', 'cas/train'] as any
 })
